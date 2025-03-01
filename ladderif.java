d879075dc96d5d1ff2e28194e621c5e588212225
Rajveersinh2906 <chavdarajveersinh203@gmail.com>
2025-03-01 10:35:47 +0530

    01-03-2025

---
+++ ladderif.java
@@ -0,0 +1,15 @@
+package com.basic;
+
+import java.util.Scanner;
+
+public class ladderif {
+	public static void main(String[] args) {
+		int rno,s1,s2,s3,total;
+		double per;
+		String sname;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter name of the student :");
+		
+	}
+
+}
